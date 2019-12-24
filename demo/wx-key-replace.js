e72fe977d8756cf2400c227afca247c0a2a6653e
zhouhuafei <1123486116@qq.com>
2019-12-24 19:05:34 +0800

    feat: 🎸 demo

--- demo/wx-key-replace.js
+++ demo/wx-key-replace.js
@@ -1,13 +1,3 @@
 module.exports = {
-  files: `./src/**/**.wxml`,
-  replace: [ // 无此项或此项为空数组，则开启自动匹配模式
-    {
-      source: `wx:key="{{index}}"`,
-      target: `wx:key="index"`
-    },
-    {
-      source: `wx:key="{{ item.id }}"`,
-      target: `wx:key="id"`
-    }
-  ]
+  files: `./src/**/**.wxml`
 }
