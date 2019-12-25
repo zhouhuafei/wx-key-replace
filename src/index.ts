4726eb3fcce98a18d6d8c4d34e8c1807b305e91f
zhouhuafei <1123486116@qq.com>
2019-12-25 14:51:55 +0800

    fix: 🐛 fileContent

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,47 @@
 const fs = require('fs')
 const glob = require('glob')
 
+function fnMain (opts, fileContent) {
+  if (opts.replace && opts.replace.length) {
+    opts.replace.forEach(v => {
+      fileContent = fileContent.replace(new RegExp(v.source, 'g'), v.target)
+    })
+  } else {
+    const str = fileContent
+    const reg = 'wx:key="{{\\s*(.*?)\\s*}}"'
+    const arr = str.match(new RegExp(reg, 'g'))
+    const result = []
+    arr.forEach(v => {
+      v.match(new RegExp(reg))
+      const newStr = v.replace(RegExp.$1, `(${RegExp.$1})`)
+      str.match(new RegExp(newStr))
+      const source = newStr
+      const arrSplit = RegExp.$1.split('.')
+      const target = `wx:key="${arrSplit[arrSplit.length - 1]}"`
+      if (!result.find(v => v.source === source)) result.push({ source, target })
+    })
+    result.forEach(v => {
+      fileContent = fileContent.replace(new RegExp(v.source, 'g'), v.target)
+    })
+  }
+  return fileContent
+}
+
 module.exports = (opts) => {
   if (!opts || (!opts.files && !opts.fileContent)) {
     console.log('参数有误')
     return
   }
-  // console.log('opts', opts)
-  glob(opts.files, (err, files) => {
-    if (err) console.log('err', err)
-    // console.log('files', files)
-    files.forEach(file => {
-      let fileContent = fs.readFileSync(file, { encoding: 'utf-8' })
-      if (opts.fileContent) fileContent = opts.fileContent
-      if (opts.replace && opts.replace.length) {
-        opts.replace.forEach(v => {
-          fileContent = fileContent.replace(new RegExp(v.source, 'g'), v.target)
-        })
-      } else {
-        const str = fileContent
-        const reg = 'wx:key="{{\\s*(.*?)\\s*}}"'
-        const arr = str.match(new RegExp(reg, 'g'))
-        const result = []
-        arr.forEach(v => {
-          v.match(new RegExp(reg))
-          const newStr = v.replace(RegExp.$1, `(${RegExp.$1})`)
-          str.match(new RegExp(newStr))
-          const source = newStr
-          const arrSplit = RegExp.$1.split('.')
-          const target = `wx:key="${arrSplit[arrSplit.length - 1]}"`
-          if (!result.find(v => v.source === source)) result.push({ source, target })
-        })
-        result.forEach(v => {
-          fileContent = fileContent.replace(new RegExp(v.source, 'g'), v.target)
-        })
-        // console.log('result', result)
-        // console.log('fileContent', fileContent)
-      }
-      fs.writeFileSync(file, fileContent, { encoding: 'utf-8' })
+  if (opts.files) {
+    glob(opts.files, (err, files) => {
+      if (err) console.log('err', err)
+      files.forEach(file => {
+        let fileContent = fs.readFileSync(file, { encoding: 'utf-8' })
+        fileContent = fnMain(opts, fileContent)
+        fs.writeFileSync(file, fileContent, { encoding: 'utf-8' })
+      })
     })
-  })
+  } else if (opts.fileContent) {
+    return fnMain(opts, opts.fileContent)
+  }
 }
